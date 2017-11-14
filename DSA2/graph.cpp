bfdeaf379a73908f1a917a15a30f1df9455ece6d
ceegeechow <camille@chowww.com>
2017-11-13 20:59:43 -0500

    dijstra and insert coded

--- DSA2/graph.cpp
+++ DSA2/graph.cpp
@@ -1,26 +1,72 @@
 #include "graph.h"
-#include <climits> //limits.h?
+#include "heap.h"
+#include <stdio.h>
 
-graph::graph()
+void graph::insert(const string &name1, const string &name2, int c)
 {
-    size = 0;
-    mapping = new hashTable(0);
+    //add vertices that don't already exist in graph
+    if (!mapping->contains(name1))
+    {
+        node n;
+        n.name = name1;
+        V.push_back(n);
+        mapping->insert(name1, &V.back());
+    }
+    if (!mapping->contains(name2))
+    {
+        node n;
+        n.name = name2;
+        V.push_back(n);
+        mapping->insert(name2, &V.back());
+    }
+    //add edge with destination name2 and cost c
+    node *p1 = static_cast<node *> (mapping->getPointer(name1));
+    node *p2 = static_cast<node *> (mapping->getPointer(name2));
+    edge e;
+    e.dest = p2;
+    e.cost = c;
+    p1->adj.push_back(e);
 }
 
-int graph::insert(const std::string &name)
+void graph::dijkstra(const string &s)
 {
-    if (mapping->contains(name))
-        return 1;
-    node N = new node();
-    N.d = INT_MAX;
-    N.p = NULL;
-    N.known = false;
-    V.push_back(N);
-    mapping->insert(name,);//need pointer here
+    heap H = heap(V.size());
+    node *ps = static_cast<node *> (mapping->getPointer(s));
+    ps->d = 0;
+    for (node n : V)
+    {
+       H.insert(n.name, n.d, &n);
+    }
+    node* pv;
+    while (H.deleteMin(NULL, NULL, &pv) == 0)
+    {
+        pv->known = true;
+        for (edge e : pv->adj)
+        {
+            node* pw = e.dest;
+            if (!(pw->known) && (pv->d + e.cost < pw->d))
+            {
+                pw->d = pv->d + e.cost;
+                H.setKey(pw->name, pw->d);
+                pw->p = pv;
+            }
+        }
+    }
+}
+
+int graph::output(const string fname)
+{
+//    FILE* outFile = fopen(fname,"w");
+//    if (outFile == NULL)
+//    {
+//        fprintf(stderr, "Error openning output file '%s'\n", fname);
+//        return -1;
+//    }
+//    
+//    fclose(outFile);
     return 0;
 }
 
-void graph::getData()
-{
-    
+int main() {
+    return 0;
 }
--- DSA2/graph.h
+++ DSA2/graph.h
@@ -4,43 +4,43 @@
 #include "hash.h"
 #include <string>
 #include <list>
+#include <climits>
 using namespace std;
 
 class graph {
     
 public:
-    
-    // graph constructor: initializes graph with size 0
-    // and initializes hashTable of smallest size
-    graph();
-    
-    // insert: inserts vertex into graph with given name
+    // insert: inserts vertex/edge into graph with given name
+    void insert(const string &name1, const string &name2, int c);
+    // dijkstra: runs algorithm on graph
+    void dijkstra(const string &s);
+    // prints results to specified output file
     // returns:
     //  0 on success
-    //  1 if node already exists in graph
-    int insert(const std::string &name);
-    void getData();
+    //  -1 on fopen error
+    int output(const string fname);
     
 private:
+    
+    class edge;
     
     class node {
     public:
         string name; // The name of this node
-        node* p; //pointer to previous node
-        int d; // dv for dijkstra
-        bool known;
-        list<edge> *adj; // Adjacency list
+        node* p = NULL; //pointer to previous node
+        int d = INT_MAX; // dv for dijkstra
+        bool known = false;
+        list<edge> adj; // Adjacency list
     };
     
     class edge {
     public:
         node* dest; // destination node
-        int c; // cost of edge
-    }
+        int cost; // cost of edge
+    };
     
     list<node> V; // List of vertices in graph
     hashTable *mapping; // mapping of vertices
-    int size; //number of vertices in graph
 };
 
 #endif //_GRAPH_H
